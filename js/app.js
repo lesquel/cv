1b1afa1ae8fb130abd3185526af3e10fac285ad5
lesquel <lesquel1319@gmail.com>
2025-05-04 10:45:04 -0500

    yaaa

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-import { getInfoData } from "./services/get-info-data.js";
 import { useComponent } from "./services/use-component.js";
 
-
-useComponent()
-
+useComponent();
--- js/services/use-component.js
+++ js/services/use-component.js
@@ -1,20 +1,15 @@
-const getComponents = () => {
-  const components = document.querySelectorAll(`[data-component]`);
-  return components;
-};
+const getComponents = () => document.querySelectorAll(`[data-component]`);
 
 const getDocumentComponents = async (name) => {
   const rest = await fetch(`/components/${name}.html`);
-  const data = await rest.text();
-  return data;
+  return await rest.text();
 };
 
 export const useComponent = () => {
   const components = getComponents();
 
   components.forEach(async (component) => {
-    const componentName = component.getAttribute("data-component");
-    const componentData = await getDocumentComponents(componentName);
+    const componentData = await getDocumentComponents(component.getAttribute("data-component"));
     component.outerHTML = componentData;
   });
 };
